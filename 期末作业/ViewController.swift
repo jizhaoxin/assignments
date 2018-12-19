31b541093cf1f8c379f59b1e81e3d7128ddf7bb8
季兆欣 <jizhaoxin@jizhaoxindemacbook-pro.local>
2018-12-19 14:24:06 +0800

    Initial Commit

---
+++ 期末作业/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  期末作业
+//
+//  Created by 季兆欣 on 2018/12/19.
+//  Copyright © 2018年 季兆欣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
